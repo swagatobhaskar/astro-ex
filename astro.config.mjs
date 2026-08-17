8fd7d3f9c22c6ffd5f8b8578ac1095fcbf6be24b
Swagato Bhaskar <bhaskarswagato@gmail.com>
2026-08-17 20:41:37 +0530

    ADD: Preact island. Dark/light toggle.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://neon-madeleine-8e18de.netlify.app"
+  site: "https://neon-madeleine-8e18de.netlify.app",
+  integrations: [preact()]
 });
